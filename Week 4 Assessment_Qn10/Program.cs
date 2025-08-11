d4376c991eb3cabc141be582b336ece5a9c62e51
KAVIARASU S <105537778+kaviarasu1811@users.noreply.github.com>
2025-08-11 15:50:27 +0530

    Add files via upload

---
+++ Week 4 Assessment_Qn10/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class StackNode
+{
+    public int Data;
+    public StackNode? Next; // Nullable
+    public StackNode(int data) { Data = data; Next = null; }
+}
+
+class Stack
+{
+    private StackNode? top; // Nullable
+
+    public void Push(int data)
+    {
+        StackNode newNode = new StackNode(data);
+        newNode.Next = top;
+        top = newNode;
+    }
+
+    public void Pop()
+    {
+        if (top == null)
+        {
+            Console.WriteLine("Stack is empty");
+            return;
+        }
+        top = top.Next;
+    }
+
+    public void DisplayTop()
+    {
+        Console.WriteLine($"Top: {(top != null ? top.Data : -1)}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stack stack = new Stack();
+
+        stack.Push(10);
+        stack.Push(20);
+        stack.Push(30);
+
+        stack.DisplayTop(); // Top: 30
+
+        stack.Pop();
+        stack.DisplayTop(); // Top: 20
+
+        stack.Pop();
+        stack.Pop();
+        stack.Pop(); // "Stack is empty"
+    }
+}
